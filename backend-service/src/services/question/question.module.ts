8bc3c895dae64397bb7379391f804eba1b436feb
Danil Kladnitsky <danyakladnitsky@gmail.com>
2023-11-30 16:08:14 +0300

    fix question module

--- backend-service/src/services/question/question.module.ts
+++ backend-service/src/services/question/question.module.ts
@@ -1,7 +1,12 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { QuestionEntity } from "../../database/entities/question.entity";
 import { QuestionService } from "./question.service";
 
 @Module({
+    imports: [
+        TypeOrmModule.forFeature([QuestionEntity]),
+    ],
     providers: [QuestionService],
     exports: [QuestionService]
 })
